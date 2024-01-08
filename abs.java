1495883854b3058076cf5fe1ab61776a1e0a1555
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-08 14:43:28 +0000

    Add files via upload

---
+++ abs.java
@@ -0,0 +1,11 @@
+public class abs {
+  static int abs(int i)
+  {
+    return (i < 0) ? -i : i;
+  }
+	public static void main(String []args){
+		System.out.println("\033c\033[43;37m{}\n");	
+		System.out.println(abs(-1));
+		
+	}
+}
---
+++ class2java.sh
@@ -0,0 +1,2 @@
+printf "\ec\e[43;37m\a\n\n"
+procyon $1 
